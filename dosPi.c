60f70bbde20eccd5f07e016849ffead987cde5dc
John Doe~ <coazo@pgmail.com>
2025-09-17 14:59:16 +0000

    INTENTO 1

---
+++ dosPi.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <math.h>
+
+int main(void) {
+    /**
+     * Tabla de resultados de la primera sucesión.
+     */
+
+    int k = 10;
+    float s4 = sqrtf(2);
+
+    int sk_1, sk_2;
+
+    for (int i = 2; i < 2*k ; i+=2) {
+
+        printf("||\tNúmero  de vértices: %d\t||\tValor de la aproximación: %le\t||\tError relativo: %le", k, )
+    }
+
+
+    
+
+    return 0;
+}
