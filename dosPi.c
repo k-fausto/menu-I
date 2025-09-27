b70d3c566583c9fa90148800a10d68fef6e8b4a6
fausto_ <karimbm@me.com>
2025-09-27 02:58:33 +0200

    dospi acabado

--- dosPi.c
+++ dosPi.c
@@ -3,27 +3,39 @@
 
 int main(void) {
     // Haremos t comprobaciones.
-    int t = 15;
+    int t = 28;
+    
+    float PI = acosf(-1.0);
+    double d_PI = acos(-1.0);
+
+    // Creemos el archivo que gnuplot leerá.
+
+    FILE *f1, *f2, *f3, *f4, *f5;
+    f1 = fopen("Sk_simple.dat", "w");
+    f2 = fopen("Sk_doble.dat", "w");
+    f3 = fopen("Ck_simple.dat", "w");
+    f4 = fopen("Ck_doble.dat", "w");
+    f5 = fopen("Sk_sin_cancelacion.dat", "w");
 
     /**
      * Tabla de resultados de la primera sucesión, sk, con precisión simple.
      */
-    float PI = acosf(-1.0);
     float sk_0, sk_1, er;
     int k = 0;
     sk_0 = sqrtf(2);
 
     printf("============================================================================\n");
-    printf("||\tAproximacion de pi usando sk, y precision simple.\n");
+    printf("||\tAproximacion de dos veces pi usando sk, y precision simple.\n");
     printf("============================================================================\n");
 
     for (int i = 3; i <= t ; i++) {
         k = pow(2,i);
         sk_1 = sqrtf(2 - sqrtf(4 - powf(sk_0, 2)));
-        er = fabsf((sk_1 * k - 2 * PI) / (2 * PI)); // Corregidos los paréntesis
+        er = fabsf((sk_1 * k - 2 * PI) / (2 * PI)); 
 
-        printf("||\tNumero de vertices: %7d \t ||\tValor de la aproximacion: %le \t || \tError relativo: %le%%.\n", k/2, (double)(sk_1 * k), (double)(er * 100)); // Cast a double para %le
+        printf("||\tNumero de vertices: %7d \t ||\tValor de la aproximacion: %.20le \t || \tError relativo: %.20le%%.\n", k/2, sk_1 * k, er * 100);
         sk_0 = sk_1;
+        fprintf(f1, "%d %f\n", k/2, er*100);
     }
 
     printf("============================================================================\n\n");
@@ -32,22 +44,95 @@ int main(void) {
      * Tabla de resultados con precisión doble.
      */
     printf("============================================================================\n");
-    printf("||\tAproximacion de pi usando sk, y precision doble.\n");
+    printf("||\tAproximacion de dos veces pi usando sk, y precision doble.\n");
     printf("============================================================================\n");
 
-    double d_PI = acos(-1.0);
+    
     double d_sk_0, d_sk_1, d_er;
     d_sk_0 = sqrt(2);
 
     for (int i = 3; i <= t ; i++) {
         k = pow(2,i);
         d_sk_1 = sqrt(2 - sqrt(4 - pow(d_sk_0, 2)));
-        d_er = fabs((d_sk_1 * k - 2 * d_PI) / (2 * d_PI)); // Corregidos los paréntesis
+        d_er = fabs((d_sk_1 * k - 2 * d_PI) / (2 * d_PI));
 
-        printf("||\tNumero de vertices: %7d \t ||\tValor de la aproximacion: %le \t || \tError relativo: %le%%.\n", k/2, d_sk_1 * k, d_er * 100);
+        printf("||\tNumero de vertices: %7d \t ||\tValor de la aproximacion: %.20le \t || \tError relativo: %.20le%%.\n", k/2, d_sk_1 * k, d_er * 100);
         d_sk_0 = d_sk_1;
+        fprintf(f2, "%d %le\n", k/2, d_er*100);
     }
     printf("============================================================================\n\n");
+    
+    
+    
+    /**
+     * Tabla de resultados de la primera sucesión, ck, con precisión simple.
+     */
+    float ck;
+    sk_0 = sqrtf(2);
+    
+    printf("============================================================================\n");
+    printf("||\tAproximacion de dos veces pi usando ck, y precision simple.\n");
+    printf("============================================================================\n");
+    
+    for (int i = 3; i <= t ; i++) {
+        k = pow(2,i);
+        sk_1 = sqrtf(2 - sqrtf(4 - powf(sk_0, 2)));
+        
+        ck = (2*sk_1)/(sqrtf(4-powf(sk_1, 2)));
+        
+        er = fabsf((ck * k - 2 * PI) / (2 * PI)); 
+        
+        printf("||\tNumero de vertices: %7d \t ||\tValor de la aproximacion: %.20le \t || \tError relativo: %.20le%%.\n", k/2, ck * k, er * 100);
+        sk_0 = sk_1;
+        fprintf(f3, "%d %f\n", k/2, er*100);
+    }
+    
+    printf("============================================================================\n\n");
+    
+    /**
+     * Tabla de resultados con precisión doble.
+     */
+    printf("============================================================================\n");
+    printf("||\tAproximacion de dos veces pi usando ck, y precision doble.\n");
+    printf("============================================================================\n");
+    
+    double d_ck;
+    d_sk_0 = sqrt(2);
+    
+    for (int i = 3; i <= t ; i++) {
+        k = pow(2,i);
+        d_sk_1 = sqrt(2 - sqrt(4 - pow(d_sk_0, 2)));
+        d_er = fabs((d_ck * k - 2 * d_PI) / (2 * d_PI)); 
+        d_ck = (2*d_sk_1)/(sqrt(4-pow(d_sk_1, 2)));
+        
+        printf("||\tNumero de vertices: %7d \t ||\tValor de la aproximacion: %.20le \t || \tError relativo: %.20le%%.\n", k/2, d_sk_1 * k, d_er * 100);
+        d_sk_0 = d_sk_1;
+        fprintf(f4, "%d %le\n", k/2, d_er*100);
+    }
+    printf("============================================================================\n\n");
+
+    // Con sk mejorado.
+    printf("============================================================================\n");
+    printf("||\tAproximacion de dos veces pi usando sk sin cancelacion, y precision doble.\n");
+    printf("============================================================================\n");
+
+    double sk = 0.0;
+    for (int i = 2; i <= t; i++) {
+        k = pow(2,i);
+        sk = 2.0 * sin(d_PI/k);
+        d_er = fabs((sk * k - 2.0 * d_PI) / (2.0 * d_PI)); 
+
+        printf("||\tNumero de vertices: %11d \t ||\tValor de la aproximacion: %.20le \t || \tError relativo: %.20le%%.\n", k, sk * k, d_er * 100);
+        fprintf(f5, "%d %f\n", k, d_er*100);
+    }
+
+    printf("============================================================================\n\n");
+
+    fclose(f1);
+    fclose(f2);
+    fclose(f3);
+    fclose(f4);
+    fclose(f5);
 
     return 0;
 }
