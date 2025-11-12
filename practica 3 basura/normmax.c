f6be6f362dee0ba451264f87ae396492f09289af
John Doe~ <johndoe@example.com>
2025-11-12 15:01:56 +0000

    donv por ahoy

---
+++ practica 3 basura/normmax.c
@@ -0,0 +1,24 @@
+/**
+ * Karim Boujana Marcucci.
+ */
+
+#include <math.h> 
+
+double normMax(int m, double **matA) {
+    /**
+     * Clalcula la norma máxima de una matriz.
+     */
+    double maxRowSum = 0.0;
+
+    for (int i = 0; i < m; i++) {
+        /* Sumo todos los elementos de la fila en valor absoluto, y me voy quedando con los máximos.*/
+        double rowSum = 0.0;
+        for (int j = 0; j < m; j++) rowSum += fabs(matA[i][j]);
+        if (rowSum > maxRowSum) maxRowSum = rowSum;
+        
+    }
+
+    return maxRowSum;
+
+
+}
